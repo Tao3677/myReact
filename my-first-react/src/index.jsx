7c1fc5ff6b9207c879b9eb072bb4a0bd4505a54a
魏永涛 <weiyongtao@windward.com.cn>
2021-01-26 12:32:28 +0800

    使用脚手架

---
+++ my-first-react/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <h1>Hello World</h1>
+      <span>这是一个span标签</span>
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
